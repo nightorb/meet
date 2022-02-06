24a362c286279ec48b32eb65677b5a22367e22f0
nightorb <89855337+nightorb@users.noreply.github.com>
2022-02-06 19:52:53 +0100

    update event.js

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
 
 class Event extends Component {
+  state = {
+    collapsed: true
+  }
+
+  handleButtonClick = () => {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+
   render() {
-    return <div></div>
+    const { event } = this.props;
+
+    return (
+      <div className="event">
+        <h3 className="summary">{event.summary}</h3>
+        <p className="dateTime">{event.dateTime}</p>
+        <p className="timeZone">{event.timeZone}</p>
+        <p className="location">{event.location}</p>
+        <button className="show-details" onClick={this.handleButtonClick}>show details</button>
+
+        <div className="event-details">
+          <a href={event.htmlLink} className="htmlLink">See details on Google Calendar</a>
+          <p className="description">{event.description}</p>
+          <button className="hide-details" onClick={this.handleButtonClick}>hide details</button>
+        </div>
+      </div>
+    );
   }
 }
 
