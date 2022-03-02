5a6e9c252b5b258d9c84fb3c12e323ce5984bb68
nightorb <ana.nath@gmx.de>
2022-03-02 22:17:08 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-import { extractLocations, getEvents } from './api';
+import WelcomeScreen from './WelcomeScreen';
+
+import { extractLocations, getEvents, checkToken, getAccessToken } from './api';
 
 import './App.css';
 import './nprogress.css';
@@ -11,13 +14,24 @@ class App extends Component {
   state = {
     events: [],
     locations: [],
-    numberOfEvents: 32
+    numberOfEvents: 32,
+    showWelcomeScreen: undefined
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     // load events when app loads, make API call and save initial data to state
     // only update state if this.mounted is true to prevent that component unmounts before API call finished
     this.mounted = true;
+
+    const accessToken = localStorage.getItem('access_token');
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get('code');
+
+    this.setState({
+      showWelcomeScreen: !(code || isTokenValid)
+    });
+
     getEvents().then((events) => {
       if (this.mounted) {
         this.setState({
@@ -50,11 +64,14 @@ class App extends Component {
   }
 
   render() {
+    if (this.state.showWelcomeScreen === undefined) return <div className="App" />
+
     return (
       <div className="App">
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
         <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateNumberOfEvents={this.updateNumberOfEvents} />
         <EventList events={this.state.events.slice(0, this.state.numberOfEvents)} />
+        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={() => { getAccessToken() }} />
       </div>
     );
   }
