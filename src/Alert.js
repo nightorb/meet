e874844486605e1d21c06148e6922df8864feb8c
nightorb <ana.nath@gmx.de>
2022-03-05 01:19:49 +0100

    update Alert

--- src/Alert.js
+++ src/Alert.js
@@ -24,14 +24,14 @@ class Alert extends Component {
 class InfoAlert extends Alert {
   constructor(props) {
     super(props);
-    this.color = 'blue';
+    this.color = '#678fe4';
   }
 }
 
 class ErrorAlert extends Alert {
   constructor(props) {
     super(props);
-    this.color = 'red';
+    this.color = '#f0a384';
   }
 }
 
