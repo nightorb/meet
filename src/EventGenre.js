aeb4711ffe92670fa3f560e5263635cfb0b2749e
nightorb <ana.nath@gmx.de>
2022-03-06 22:07:39 +0100

    create EventGenre.js
    the pie chart

---
+++ src/EventGenre.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
+
+const EventGenre = ({ events }) => {
+  const [ data, setData ] = useState([]);
+
+  useEffect(() => {
+    const getData = () => {
+      const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+      const data = genres.map((genre) => {
+        const value = events.filter(({ summary} ) =>
+          summary.split(' ').includes(genre)
+        ).length;
+        return { name: genre, value };
+      });
+      return data;
+    };
+    setData(() => getData());
+  }, [events]);
+
+  const COLORS = ['#678fe4', '#b588d5', '#e186b7', '#f39099', '#f0a384'];
+
+  return (
+    <ResponsiveContainer id="pie-container" height={400}>
+      <PieChart width={400} height={400}>
+        <Pie
+          data={data}
+          cx={200}
+          cy={200}
+          labelLine={false}
+          outerRadius={80}
+          fill="8884d8"
+          dataKey="value"
+          label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+      </PieChart>
+    </ResponsiveContainer>
+  );
+}
+
+export default EventGenre;
